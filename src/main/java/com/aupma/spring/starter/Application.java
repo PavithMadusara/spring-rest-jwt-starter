8031977bddc503b76aef7ed82f4aebe6aa8578d0
Pavith Madusara <pavithmadusara@gmail.com>
2022-06-13 20:50:19 +0530

    Add Password Encoder Bean for Bcrypt

--- src/main/java/com/aupma/spring/starter/Application.java
+++ src/main/java/com/aupma/spring/starter/Application.java
@@ -2,12 +2,21 @@ package com.aupma.spring.starter;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.core.GrantedAuthorityDefaults;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 public class Application {
 
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 
 }
