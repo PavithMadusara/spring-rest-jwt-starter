58a6e0bf0109fdf57c763f10d977d5a88a721bf3
Pavith Madusara <pavithmadusara@gmail.com>
2022-06-13 19:56:14 +0530

    Created Entity Classes for Permission,Role and User

---
+++ src/main/java/com/aupma/spring/starter/entity/User.java
@@ -0,0 +1,72 @@
+package com.aupma.spring.starter.entity;
+
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import javax.persistence.*;
+import java.time.OffsetDateTime;
+import java.util.Set;
+
+
+@Entity
+@EntityListeners(AuditingEntityListener.class)
+@Getter
+@Setter
+public class User {
+
+    @Id
+    @Column(nullable = false, updatable = false)
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String username;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column
+    private String firstName;
+
+    @Column
+    private String lastName;
+
+    @Column
+    private Boolean isTempPassword;
+
+    @Column
+    private Boolean isBanned;
+
+    @Column
+    private Boolean isApproved;
+
+    @ManyToMany(cascade = {CascadeType.PERSIST, CascadeType.MERGE}, fetch = FetchType.EAGER)
+    @JoinTable(
+            name = "user_roles",
+            joinColumns = @JoinColumn(name = "user_id"),
+            inverseJoinColumns = @JoinColumn(name = "role_id")
+    )
+    private Set<Role> roles;
+
+    @CreatedDate
+    @Column(nullable = false, updatable = false)
+    private OffsetDateTime dateCreated;
+
+    @LastModifiedDate
+    @Column(nullable = false)
+    private OffsetDateTime lastUpdated;
+
+    @PrePersist
+    public void onCreate() {
+        dateCreated = lastUpdated = OffsetDateTime.now();
+    }
+
+    @PreUpdate
+    public void onUpdate() {
+        lastUpdated = OffsetDateTime.now();
+    }
+
+}
