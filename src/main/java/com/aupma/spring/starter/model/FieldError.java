cacbcb1411434bcdee72419d4895b1996a3ae955
Pavith Madusara <pavithmadusara@gmail.com>
2022-06-16 14:33:42 +0530

    Add REST exception handler

---
+++ src/main/java/com/aupma/spring/starter/model/FieldError.java
@@ -0,0 +1,13 @@
+package com.aupma.spring.starter.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class FieldError {
+
+    private String field;
+    private String errorCode;
+
+}
