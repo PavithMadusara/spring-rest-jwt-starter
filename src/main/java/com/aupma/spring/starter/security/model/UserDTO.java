c0068a80f88b6b838ea2d0522fb554c0f76f4466
Pavith Madusara <pavithmadusara@gmail.com>
2022-06-21 11:04:24 +0530

    UserDTO implements UserDetails

--- src/main/java/com/aupma/spring/starter/security/model/UserDTO.java
+++ src/main/java/com/aupma/spring/starter/security/model/UserDTO.java
@@ -2,15 +2,19 @@ package com.aupma.spring.starter.security.model;
 
 import lombok.Getter;
 import lombok.Setter;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
 
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.Collection;
 import java.util.List;
 
 
 @Getter
 @Setter
-public class UserDTO {
+public class UserDTO implements UserDetails {
 
     private Long id;
 
@@ -47,5 +51,34 @@ public class UserDTO {
     private Boolean isApproved;
 
     private List<RoleDTO> roles;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        List<GrantedAuthority> authorities = new ArrayList<>();
+        roles.forEach(role -> role.getPermissions().forEach(permission ->
+                authorities.add((GrantedAuthority) permission::getCode))
+        );
+        return authorities;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return isApproved;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return !isBanned;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return isApproved;
+    }
 
 }
